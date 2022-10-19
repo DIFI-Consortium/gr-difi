767280030d0491778b181f79d2d3ee1c1576594e
David Milovich <david.milovich@welkinsciences.com>
2022-10-19 13:20:52 -0600

    corrected third nibble of context packet header word

--- lib/difi_sink_cpp_impl.cc
+++ lib/difi_sink_cpp_impl.cc
@@ -70,7 +70,7 @@ namespace gr {
       d_full = reference_time_full;
       d_frac = reference_time_frac;
       d_static_bits = 0x18e00000; // default, will change after first packet else, this is the DIFI standard first 12 bits
-      d_context_static_bits = 0x49000000;// default, will change after first packet else, this is the DIFI standard first 8 bits
+      d_context_static_bits = 0x49e00000;// header bits 31-20 must be 0x49e (posix), 0x49a (gps), or 0x496 (utc)
       d_unpack_idx_size = bit_depth == 8 ? 1 : 2;
       d_samples_per_packet = samples_per_packet;
       d_time_adj = (double)d_samples_per_packet / samp_rate;
